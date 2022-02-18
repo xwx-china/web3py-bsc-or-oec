184e71e1065885ba8462ca491be59fd98830d8c9
xwx-china <xwx19961220@gmail.com>
2022-02-18 10:01:27 +0800

    提交加密刀片全自动脚本，解放双手

--- oec/isl_auto.py
+++ oec/isl_auto.py
@@ -5,29 +5,28 @@ from datetime import datetime,timedelta
 import time
 
 isl = Isl()
-
-w3 = isl.web3Utils.w3
-
-# # 获取key
-# key = AdminKey.KEY.value
-# # 获取账号
-# account = isl.web3Utils.get_account(key)
-# # 获取上一次进行游戏时间
-# tm = isl.gameHarvestContract.functions.islLastTime(account.address).call()
-# # 下一次进行游戏时间
-# timeObj = datetime.fromtimestamp(tm) + timedelta(hours=3)
-# # 转换成时间戳
-# timeMk = int(timeObj.timestamp())
-# # 获取当前时间
-# timeMkNow = int(time.time())
-# # 判断当前时间大于游戏时间开始游戏
-# if timeMkNow > timeMk:
-#     # 获取key
-#     key = AdminKey.KEY.value
-#     # 获取账号
-#     account = isl.web3Utils.get_account(key)
-#     # 获取交易次数
-#     nonce = isl.web3Utils.get_nonce(account.address)
-#     # 发送交易
-#     tx = isl.play_game_harvest(key, nonce)
-#     print(tx)
+# 获取key
+key = AdminKey.KEY.value
+# 获取账号
+account = isl.web3Utils.get_account(key)
+# 获取上一次进行游戏时间
+tm = isl.gameHarvestContract.functions.islLastTime(account.address).call()
+# 下一次进行游戏时间
+timeObj = datetime.fromtimestamp(tm) + timedelta(hours=3)
+# 转换成时间戳
+timeMk = int(timeObj.timestamp())
+# 获取当前时间
+timeMkNow = int(time.time())
+print(timeMk)
+print(timeMkNow)
+# 判断当前时间大于游戏时间开始游戏
+if timeMkNow > timeMk:
+    # 获取key
+    key = AdminKey.KEY.value
+    # 获取账号
+    account = isl.web3Utils.get_account(key)
+    # 获取交易次数
+    nonce = isl.web3Utils.get_nonce(account.address)
+    # 发送交易
+    # tx = isl.play_game_harvest(key, nonce)
+    # print(tx)
--- oec/skill_auto.py
+++ oec/skill_auto.py
@@ -1,25 +1,107 @@
 from oec.skill_utils import Skill
-from oec.oec_key import AdminKey
+from oec.oec_key import AdminKey,SkillKey
+
+import threading
+import time
+
+import math
+
+import logging
 
 skill = Skill()
+# 日志
+logger = logging.getLogger()
+logfile = 'skill.log'
+hdlr = logging.FileHandler(logfile)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
 
-key = AdminKey.KEY.value
 
-account = skill.web3Utils.get_account(key)
+def get_cbcs(address):
+    '''
+    获取角色
+    '''
+    cbcs = skill.cbcContract.functions.getCharactersOwnedBy(address).call()
+    return cbcs
 
-nonce = skill.web3Utils.get_nonce(account.address)
+def get_cbw(address):
+    '''
+    获取武器
+    '''
+    cbw = skill.cbwContract.functions.tokenOfOwnerByIndex(address, 0).call()
+    return cbw
 
-cnt = '3'
+def get_game_keys():
+    '''
+    获取游戏钱包
+    '''
+    keys = []
+    for keyE in AdminKey:
+        keys.append(keyE.value)
+    for keyE in SkillKey:
+        keys.append(keyE.value)
+    return keys
 
-tx = skill.play_game(key, nonce, '30472', '79452', '2', cnt)
-nonce = nonce + 1
+def start():
+    '''
+    开始游戏
+    '''
+    logger.info('脚本开始')
+    keys = get_game_keys()
+    txs = []
+    for key in keys:
+        account = skill.web3Utils.get_account(key)
+        logger.info('钱包地址:'+account.address)
+        cbcs = get_cbcs(account.address)
+        cbw = get_cbw(account.address)
+        nonce = skill.web3Utils.get_nonce(account.address)
+        for cbc in cbcs:
+            point = skill.cbcContract.functions.getStaminaPoints(cbc).call()
+            logger.info('角色:' + str(cbc) + ' 体力' + str(point))
+            # 判断角色体力
+            if point > 120:
+                # 进攻次数
+                cnt = str(math.floor(point/40))
+                # 获取对战怪物
+                targets = skill.gameContract.functions.getTargets(cbc, cbw).call()
+                monsterPowers = []
+                givetokens = []
+                # 遍历怪物
+                for target in targets:
+                    # 获取怪物战斗力
+                    monsterPower = skill.gameContract.functions.getMonsterPower(target).call()
+                    # 获取怪物奖励
+                    givetoken = skill.gameContract.functions.getTokenGainForFight(monsterPower, bool(1)).call()
+                    monsterPowers.append(monsterPower)
+                    givetokens.append(givetoken)
+                # 获取最高战斗力怪物
+                maxMonsterPower = max(monsterPowers)
+                maxMonsterPowerIndex = monsterPowers.index(maxMonsterPower)
+                # 获取最高奖励怪物
+                givetoken = max(givetokens)
+                maxGivetokenIndex = givetokens.index(givetoken)
+                # 进攻角色
+                cbcStr = str(cbc)
+                # 进攻武器
+                cbwStr = str(cbw)
+                # 判断最高奖励怪物是不是最高战斗力怪物
+                if maxGivetokenIndex == maxMonsterPowerIndex:
+                    # 是的话直接攻击
+                    tx = skill.play_game(key, nonce, cbcStr, cbwStr, str(maxGivetokenIndex), cnt)
+                    txs.append(tx)
+                    nonce = nonce + 1
+                # 不是最高战斗力怪物攻击最高奖励
+                else:
+                    tx = skill.play_game(key, nonce, cbcStr, cbwStr, str(maxGivetokenIndex), cnt)
+                    nonce = nonce + 1
+                    txs.append(tx)
+    for tx in txs:
+        logger.info('进攻tx:' + str(tx))
+    logger.info('脚本结束')
+    threading.Timer(600, start).start()
 
-tx = skill.play_game(key, nonce, '30477', '78471', '2', cnt)
-nonce = nonce + 1
 
-tx = skill.play_game(key, nonce, '5475', '79452', '2', cnt)
-nonce = nonce + 1
-
-tx = skill.play_game(key, nonce, '5467', '79452', '3', cnt)
-nonce = nonce + 1
+timer = threading.Timer(1, start).start()
 
